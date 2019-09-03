bac5badd6a3193da39c0a0805df43f57a75492ca
Simon Date <simondate@yahoo.co.uk>
2019-09-03 14:50:49 +0700

    Updated schema

--- js/adapt-splitscreen.js
+++ js/adapt-splitscreen.js
@@ -1,16 +1,8 @@
 define(['coreJS/adapt', './SplitscreenView'], function(Adapt, SplitscreenView) {
-
-    Adapt.on('pageView:preRender', function(pageView) {
-    });
-
-    Adapt.on('router:page', function(pageView) {
-    });
-
     Adapt.on('pageView:postRender', function(pageView) {
         var pageModel = pageView.model;
         if (pageModel.has('_splitscreen') && pageModel.get('_splitscreen')._isEnabled) {
             new SplitscreenView({'model': pageModel});
         }
     });
-
 });
